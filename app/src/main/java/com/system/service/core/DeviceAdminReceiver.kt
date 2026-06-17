ddd5e575f7160596e6ba48742cf017c9e7cfc0ae
godofthunder7890-crypto <godofthunder7890@gmail.com>
2026-06-18 03:19:21 +0530

    Create DeviceAdminReceiver.kt

---
+++ app/src/main/java/com/system/service/core/DeviceAdminReceiver.kt
@@ -0,0 +1,10 @@
+package com.system.service.core
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class DeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {}
+    override fun onDisabled(context: Context, intent: Intent) {}
+}
